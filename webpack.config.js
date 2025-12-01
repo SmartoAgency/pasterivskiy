15ac987811e6e8eafc571179970144ba0cd19bc9
yatsinoa <imsashasho@gmail.com>
2025-12-01 13:25:54 +0200

    path resolve

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
+const path = require('path');
 const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
   mode: process.argv.includes('--production') ? 'production' : 'development',
   entry: {
-    contacts: './src/assets/scripts/contacts.js',
-    about: './src/assets/scripts/about.js',
-    home: './src/assets/scripts/home.js',
-    index: './src/assets/scripts/index-app.js',
-    gallery: './src/assets/scripts/gallery.js',
-    developer: './src/assets/scripts/developer.js',
-    progress: './src/assets/scripts/progress.js',
-    progressSingle: './src/assets/scripts/progress-single.js',
-    notFound: './src/assets/scripts/not-found.js',
+    contacts: path.resolve(__dirname, 'src/assets/scripts/contacts.js'),
+    about: path.resolve(__dirname, 'src/assets/scripts/about.js'),
+    home: path.resolve(__dirname, 'src/assets/scripts/home.js'),
+    index: path.resolve(__dirname, 'src/assets/scripts/index-app.js'),
+    gallery: path.resolve(__dirname, 'src/assets/scripts/gallery.js'),
+    developer: path.resolve(__dirname, 'src/assets/scripts/developer.js'),
+    progress: path.resolve(__dirname, 'src/assets/scripts/progress.js'),
+    progressSingle: path.resolve(__dirname, 'src/assets/scripts/progress-single.js'),
+    notFound: path.resolve(__dirname, 'src/assets/scripts/not-found.js'),
   },
   output: {
     filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: /[\\/]node_modules[\\/]/,
         use: {
           loader: 'babel-loader',
           options: {
@@ -47,7 +49,7 @@ const config = {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'assets/images/',
+              outputPath: path.join('assets', 'images'), // ✅ Кросплатформений шлях
             },
           },
         ],
